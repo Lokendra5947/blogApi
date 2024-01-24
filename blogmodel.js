c388f8b1dc5de947b6ecddc8f8f37c3fc825e54c
Lokendra Singh Rathore <lokendrasinghr40@gmail.com>
2024-01-24 15:37:03 +0530

    hey

--- blogmodel.js
+++ blogmodel.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
 const blogSchema = new mongoose.Schema({
+    blogId : Number,
     name:String,
+    description:String,
+    category:String,
     image:String
 })
 module.exports = mongoose.model("blog",blogSchema)
--- index.js
+++ index.js
@@ -16,13 +16,34 @@ const storage = multer.diskStorage({
     }
 });
 const upload = multer({storage:storage}).single("image")
+
 app.post('/',async(req,res)=>{
 
     upload(req,res,async (err)=>{
-        const data = new blog({name:req.body.name,
+        const data = new blog({
+            blogId:req.body.blogId,
+            name:req.body.name,
+            description:req.body.description,
+            category:req.body.category,
             image:"localhost:4000/uploads/"+req.file.filename
     })
      await data.save()
     res.send("file upladed")
 })
-}).listen(4000)
+})
+
+app.get("/",async(req,res)=>{
+    const data = await blog.find()
+    res.send(data) 
+})
+
+// app.put("/update",async(req,res)=>{
+//     let update = await blog.updateOne({name:req.body.name},{$set:{name:req.body.name,image:req.body.image}})
+//     res.send({message:"Update Successfully!!", data:update})
+// })
+
+// app.delete("/delete",async(req,res)=>{
+//     let deletee = await blog.deleteOne({name:req.body.name})
+// })
+
+app.listen(4000)
